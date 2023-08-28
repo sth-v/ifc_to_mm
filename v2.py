dc2941869f28e80a3cea03e22086cc34b211ae5d
sth-v <aa@contextmachine.online>
2023-08-28 14:40:08 +0300

    clean

--- v2.py
+++ v2.py
@@ -12,33 +12,33 @@ from mmcore.base.geom.utils import create_buffer_from_dict
 NAME = "export"
 EXPORT_PATH = f"{os.getcwd()}/tmp"
 
-
-UUID = NAME.lower().replace("-","_")
-mep = AGroup(name=NAME,uuid=UUID)
+UUID = NAME.lower().replace("-", "_")
+mep = AGroup(name=NAME, uuid=UUID)
 DISABLE_TRIANGULATION = True
 NO_WIRE_INTERSECTION_CHECK = True
 
+mats = {}
+gms = {}
+colors = {}
+nms = {}
 
 
-mats={}
-gms={}
-colors={}
-nms={}
 def props(f):
     props = dict(context=f.data.context, type=f.data.type, parent_id=f.data.parent_id)
     for k in f.data.product.get_attribute_names():
-        val=f.data.product.get_argument(k)
-
+        val = f.data.product.get_argument(k)
 
         props[k] = f'{val}'
 
     return props
 
-from mmcore.base.registry import objdict, amatdict,ageomdict
 
-cb=lambda nam:nam.split(":")[0]
+from mmcore.base.registry import objdict, amatdict, ageomdict
 
-def thr(source, path=EXPORT_PATH,prefix=NAME):
+cb = lambda nam: nam.split(":")[0]
+
+
+def thr(source, path=EXPORT_PATH, prefix=NAME):
     fl = ifcopenshell.open(source)
     settings = geom.settings(USE_PYTHON_OPENCASCADE=True, DISABLE_TRIANGULATION=True,
                              NO_WIRE_INTERSECTION_CHECK=NO_WIRE_INTERSECTION_CHECK)
@@ -50,20 +50,23 @@ def thr(source, path=EXPORT_PATH,prefix=NAME):
             _name = cb(o.data.name)
             if _name not in nms.keys():
                 nms[_name] = AGroup(name=_name, uuid=_name)
-                mep.add(nms[_name] )
-                colors[_name] = ColorRGB(int(random.random() * 200), int(random.random() * 200),int(random.random() * 200)).decimal
-                amatdict[hex(hash(_name))+"_mat"] = AMesh.material_type(uuid=hex(hash(_name))+"_mat", color=colors[_name])
+                mep.add(nms[_name])
+                colors[_name] = ColorRGB(int(random.random() * 200), int(random.random() * 200),
+                                         int(random.random() * 200)).decimal
+                amatdict[hex(hash(_name)) + "_mat"] = AMesh.material_type(uuid=hex(hash(_name)) + "_mat",
+                                                                          color=colors[_name])
             gmuid = uuid.uuid4().hex
-
 
             tess = ShapeTesselator(o.geometry)
             tess.Compute(compute_edges=False,
                          mesh_quality=1.0,
                          parallel=True)
 
-            ageomdict[gmuid]=create_buffer_from_dict(json.loads(tess.ExportShapeToThreejsJSONString(gmuid)))
+            ageomdict[gmuid] = create_buffer_from_dict(json.loads(tess.ExportShapeToThreejsJSONString(gmuid)))
 
-            nms[_name].add(AMesh(name=o.data.name, geometry=ageomdict[gmuid],material=amatdict[hex(hash(_name))+"_mat"], uuid=str(o.data.id), properties=props(o)))
+            nms[_name].add(
+                AMesh(name=o.data.name, geometry=ageomdict[gmuid], material=amatdict[hex(hash(_name)) + "_mat"],
+                      uuid=str(o.data.id), properties=props(o)))
 
 
         except Exception as err:
@@ -72,19 +75,20 @@ def thr(source, path=EXPORT_PATH,prefix=NAME):
     dump_all_to_fs(path=path, prefix=prefix)
     print("All Done")
 
+
 def dump_group(name):
     return nms[name].root()
+
 
 def dump_all_to_fs(path, prefix=NAME):
     if not os.path.exists(path + "/" + prefix):
         os.mkdir(path + "/" + prefix)
     for k, n in nms.items():
-        if any([k=="", k is None]):
-
+        if any([k == "", k is None]):
             k = f"Undefined-{uuid.uuid4().hex}"
-        k=k.replace("/","-").replace("\\","-")
-        n.dump(path+"/"+prefix+"/"+k + ".json")
+        k = k.replace("/", "-").replace("\\", "-")
+        n.dump(path + "/" + prefix + "/" + k + ".json")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     thr(source='/Users/andrewastakhov/dev/W1-W4.Ifc', prefix=NAME, path=EXPORT_PATH)
