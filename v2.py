5dda3de34f3c0eafe8999a40c595470b83ae202b
sth-v <aa@contextmachine.online>
2023-08-28 14:37:39 +0300

    clean

--- v2.py
+++ v2.py
@@ -1,20 +1,15 @@
 import json
 import random
 import uuid
-
+import os
 import ifcopenshell
 from OCC.Core.Tesselator import ShapeTesselator
-from ifcopenshell.ifcopenshell_wrapper import SerializerSettings
-from ifcopenshell import geom
-from mmcore.addons.step import step_to_nodes, step_refcheck, p21,stepdct
 from mmcore.base import AGroup, AMesh
 from mmcore.geom.materials import ColorRGB
 from mmcore.base.geom.utils import create_buffer_from_dict
-from mmcore.gql.lang.parse import parse_simple_query
-NAME="W1-W4"
-import os
 
-EXPORT_PATH="/Users/andrewastakhov/PycharmProjects/viewer_test_ifc_scene/tmp"
+NAME = "export"
+EXPORT_PATH=f"{os.getcwd()}/tmp"
 
 
 UUID=NAME.lower().replace("-","_")
@@ -49,7 +44,7 @@ from mmcore.base.registry import objdict, amatdict,ageomdict
 
 cb=lambda nam:nam.split(":")[0]
 
-def thr(source='/Users/andrewastakhov/dev/W1-W4.Ifc', path=EXPORT_PATH,prefix=NAME):
+def thr(source, path=EXPORT_PATH,prefix=NAME):
     fl = ifcopenshell.open(source)
     settings = geom.settings(USE_PYTHON_OPENCASCADE=True, DISABLE_TRIANGULATION=True,
                              NO_WIRE_INTERSECTION_CHECK=NO_WIRE_INTERSECTION_CHECK)
@@ -85,7 +80,7 @@ def thr(source='/Users/andrewastakhov/dev/W1-W4.Ifc', path=EXPORT_PATH,prefix=NA
 def dump_group(name):
     return nms[name].root()
 
-def dump_all_to_fs(path="/Users/andrewastakhov/PycharmProjects/viewer_test_ifc_scene/tmp", prefix=NAME):
+def dump_all_to_fs(path, prefix=NAME):
     if not os.path.exists(path + "/" + prefix):
         os.mkdir(path + "/" + prefix)
     for k, n in nms.items():
@@ -95,17 +90,13 @@ def dump_all_to_fs(path="/Users/andrewastakhov/PycharmProjects/viewer_test_ifc_s
         k=k.replace("/","-").replace("\\","-")
         n.dump(path+"/"+prefix+"/"+k + ".json")
 
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware.gzip import GZipMiddleware
-from mmcore.base.sharedstate import serve
-serve.app.add_middleware(CORSMiddleware,allow_origins=['*'])
-serve.app.add_middleware(GZipMiddleware)
+
 if __name__=="__main__":
 
-    import threading as th
+
     #main_th=th.Thread(target=thr, args=(itr,))
     #main_th.start()
-    thr(source='/Users/andrewastakhov/dev/W1-W4.Ifc')
+    thr(source='/Users/andrewastakhov/dev/W1-W4.Ifc', prefix="W1-W4", path=EXPORT_PATH)
     #serve.start()
     #import IPython,mmcore
     #IPython.embed(header=f"[mmcore {mmcore.__version__()}]")
